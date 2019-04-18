6e0c53f07e8f1f15f8312aeb70a2afbfcb0dfc2a
John Winiata Tapsell <johntapsell@windowslive.com>
2019-04-19 10:36:11 +1200

    made api folder

---
+++ client/api/event.js
@@ -0,0 +1,30 @@
+// {/* <>
+// <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+// <script>
+
+// const username = 'badfriday'
+// const password = 'kk8yfymd37qn'
+// $.ajax({
+//   url: 'http://api.eventfinda.co.nz/v/events.json',
+//   beforeSend: function (xhr) {
+//     xhr.setRequestHeader ("Authorization", "Basic " + btoa(username + ":" + password));
+//   },
+//   success: function(xhr) {
+//     console.log(xhr);
+//   }
+// });
+// </script>
+// </> */}
+
+import request from 'superagent'
+
+const username = 'badfriday'
+const password = 'kk8yfymd37qn'
+const url = 'http://api.eventfinda.co.nz/v/events.json'
+
+export function getEvents () {
+  return request
+    .get(url)
+    .set('Authorization')
+    .then()
+}
