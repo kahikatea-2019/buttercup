f55ab55f8e7872d001e7d4a23c51b37401270473
Philip Ji <phil12p345@gmail.com>
2019-04-19 14:38:25 +1200

    weather

--- client/api/football.js
+++ client/api/football.js
@@ -1,11 +1,12 @@
 import request from 'superagent'
 
 const url = 'https://api-football-v1.p.rapidapi.com/events/{fixture_id}'
+const host = 'api-football-v1.p.rapidapi.com'
 const token = '670f61232fmsh7cdd134b6f6bad5p1d303ejsn83f423b5ea4d'
 
 export function football () {
   return request
-    .get(url + token)
+    .get(url + host + token)
     .then(res => {
       return res.fixture_id
     })
